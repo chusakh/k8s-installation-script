6ecabb0222fd7595ef861dc72c91aa572cb0b90c
chusakh <163238363+chusakh@users.noreply.github.com>
2024-03-19 13:42:18 +0700

    Update install_ceph_ubuntu_22.04.sh

--- install_ceph_ubuntu_22.04.sh
+++ install_ceph_ubuntu_22.04.sh
@@ -1,5 +1,6 @@
 sudo wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
 echo deb https://download.ceph.com/debian-reef/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
+sudo apt install python3-pip
 sudo mkdir /app
 cd /app
 sudo git clone https://github.com/ceph/ceph-deploy.git
