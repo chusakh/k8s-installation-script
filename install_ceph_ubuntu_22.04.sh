1591b5199bccd912034051430a12a915ffe5c586
chusakh <163238363+chusakh@users.noreply.github.com>
2024-03-19 13:39:42 +0700

    Create install_ceph_ubuntu_22.04.sh

---
+++ install_ceph_ubuntu_22.04.sh
@@ -0,0 +1,8 @@
+sudo wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
+echo deb https://download.ceph.com/debian-reef/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
+sudo mkdir /app
+cd /app
+sudo git clone https://github.com/ceph/ceph-deploy.git
+pip install ceph-deploy
+sudo apt  install ceph-common
+sudo apt install ceph
