37b1de0a27d788bc0bb1a0ef3147391f11baed1d
chusakh <163238363+chusakh@users.noreply.github.com>
2024-04-13 15:39:14 +0700

    Update install_k8s_common.sh

--- install_k8s_common.sh
+++ install_k8s_common.sh
@@ -33,6 +33,8 @@ crictl info
 crictl version
 
 sudo apt-get update
+sudo apt install nano
+sudo apt install iputils-ping
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
